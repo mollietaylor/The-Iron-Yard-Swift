c3a4e81595dec379c66c6a90f3df0d2b70110558
mollietaylor <mollie.taylor@gmail.com>
2015-01-26 13:34:12 -0500

    After homework

--- SwiftApp/SwiftApp/ViewController.swift
+++ SwiftApp/SwiftApp/ViewController.swift
@@ -8,8 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+let RED = UIColor.redColor()
+let SCREEN_HEIGHT = UIScreen.mainScreen().bounds.height
 
+
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var warningLabel: UILabel!
+
+    @IBAction func logInWasPressed(sender: UIButton) {
+        
+        if usernameField.text == "" || passwordField.text == "" || emailField.text == "" {
+            
+            warningLabel.text = "All fields must be filled in!"
+            
+        } else {
+            
+            warningLabel.text = "Logged in!"
+            
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
