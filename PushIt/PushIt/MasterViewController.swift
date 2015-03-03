6a8c88f17ed78a93e9fffe8b3990e713e055591c
mollietaylor <mollie.taylor@gmail.com>
2015-03-03 09:22:33 -0500

    Adds PushIt

---
+++ PushIt/PushIt/MasterViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MasterViewController.swift
+//  PushIt
+//
+//  Created by Mollie on 3/2/15.
+//  Copyright (c) 2015 Proximity Viz LLC. All rights reserved.
+//
+
+import UIKit
+
+class MasterViewController: UITableViewController {
+
+
+
+
+}
+
